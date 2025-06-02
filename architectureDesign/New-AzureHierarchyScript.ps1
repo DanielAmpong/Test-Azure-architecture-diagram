e549010d98a68c01c1b4f7db502f84e92b3d5139
Daniel Ampong <Daniel.Ampong@klp.no>
2025-06-02 16:00:18 +0200

    update repo

--- architectureDesign/New-AzureHierarchyScript.ps1
+++ architectureDesign/New-AzureHierarchyScript.ps1
@@ -1,13 +1,18 @@
-# Variable
-$outputFile = "$((Get-item -Path ".\architectureDesign\PUML").FullName)\subscriptionAzureHierarchy.puml"
-$SubscriptionName = "KLP LZ SRE playground Sandbox"
-
-if (Test-Path -Path $outputFile) { Remove-Item -Path $outputFile | Out-Null }
-
-
 # Get subscription
+$SubscriptionName = "KLP LZ SRE playground Sandbox"
 $subscription = Get-AzSubscription -SubscriptionName $SubscriptionName
 Set-AzContext -SubscriptionId $subscription.Id
+
+
+# Variable
+if (!(Test-Path -Path "architectureDesign\PUML\$($subscription.Name)")) {
+
+    New-Item -Path "$((Get-item -path "architectureDesign\PUML").FullName)" -Name "$($subscription.Name)" -ItemType "Directory" | Out-Null
+}
+
+$outputFile = "$((Get-item -Path ".\architectureDesign\PUML\$($subscription.Name)").FullName)\subscriptionAzureHierarchy.puml"
+if (Test-Path -Path $outputFile) { Remove-Item -Path $outputFile | Out-Null }
+
 
 $resourceTypeIcons = Get-Content -Path ".\architectureDesign\resourceTypeIcons.json" | ConvertFrom-Json -asHashtable
 
@@ -51,53 +56,62 @@ foreach ($resourceGroup in $resourceGroups) {
 "LAYOUT_WITH_LEGEND()" | Out-File -FilePath $outputFile -Append -Encoding utf8
 "@enduml" | Out-File -FilePath $outputFile -Append -Encoding utf8
 
+
+if (!(Test-Path -Path "architectureDesign\PUML\$($subscription.Name)_ResourceGroups")) {
+
+    New-Item -Path "$((Get-item -path "architectureDesign\PUML").FullName)" -Name "$($subscription.Name)_ResourceGroups" -ItemType Directory | Out-Null
+
+}
+
 $resourceGroupNames = (Get-AzResourceGroup).ResourceGroupName
 foreach ($resourceGroupName in $resourceGroupNames) {
+    
+    $outputFile = "$((Get-item -Path ".\architectureDesign\PUML\$($subscription.Name)_ResourceGroups").FullName)\$($resourceGroupName)AzureHierarchy.puml"
+    if (Test-Path -Path $outputFile) { Remove-Item -Path $outputFile | Out-Null }
+
+    "@startuml" | Out-File -FilePath $outputFile -Encoding utf8
+    "!pragma revision 1" | Out-File -FilePath $outputFile -Append -Encoding utf8
+    "" | Out-File -FilePath $outputFile -Append -Encoding utf8
+    "!includeurl https://raw.githubusercontent.com/plantuml-stdlib/C4-PlantUML/master/C4_Container.puml" | Out-File -FilePath $outputFile -Append -Encoding utf8
+    "" | Out-File -FilePath $outputFile -Append -Encoding utf8
+    "!define AzurePuml https://raw.githubusercontent.com/plantuml-stdlib/Azure-PlantUML/master/dist" | Out-File -FilePath $outputFile -Append -Encoding utf8
+    "!includeurl AzurePuml/AzureCommon.puml" | Out-File -FilePath $outputFile -Append -Encoding utf8
+    "!includeurl AzurePuml/AzureC4Integration.puml" | Out-File -FilePath $outputFile -Append -Encoding utf8
+    "!includeurl AzurePuml/Management/AzureResourceGroups.puml" | Out-File -FilePath $outputFile -Append -Encoding utf8
+    "" | Out-File -FilePath $outputFile -Append -Encoding utf8
+
+    $rgLabel = $resourceGroupName
+    $rgLabel_ = $rgLabel.replace("-", "_")
+    $rgLabel_ = $rgLabel_.replace(" ", "_") 
+    $rgLevel = "AzureResourceGroups($rgLabel_, '$rgLabel', 'Azure Resource Group')" 
+    $rgLevel.replace("''", """") | Out-File -FilePath $outputFile -Append -Encoding utf8
+    "" | Out-File -FilePath $outputFile -Append -Encoding utf8
 
     $resources = Get-AzResource -ResourceGroupName $resourceGroupName
     foreach ($resource in $resources) {
-        
-        $outputFile = "$((Get-item -Path ".\architectureDesign\PUML").FullName)\$($resourceGroupName)AzureHierarchy.puml"
-        if (Test-Path -Path $outputFile) { Remove-Item -Path $outputFile | Out-Null }
-
-
-        "@startuml" | Out-File -FilePath $outputFile -Encoding utf8
-        "!pragma revision 1" | Out-File -FilePath $outputFile -Append -Encoding utf8
-        "" | Out-File -FilePath $outputFile -Append -Encoding utf8
-        "!includeurl https://raw.githubusercontent.com/plantuml-stdlib/C4-PlantUML/master/C4_Container.puml" | Out-File -FilePath $outputFile -Append -Encoding utf8
-        "" | Out-File -FilePath $outputFile -Append -Encoding utf8
-        "!define AzurePuml https://raw.githubusercontent.com/plantuml-stdlib/Azure-PlantUML/master/dist" | Out-File -FilePath $outputFile -Append -Encoding utf8
-        "!includeurl AzurePuml/AzureCommon.puml" | Out-File -FilePath $outputFile -Append -Encoding utf8
-        "!includeurl AzurePuml/AzureC4Integration.puml" | Out-File -FilePath $outputFile -Append -Encoding utf8
-        "!includeurl AzurePuml/Management/AzureResourceGroups.puml" | Out-File -FilePath $outputFile -Append -Encoding utf8
-
-        $rgLabel = $resourceGroupName
-        $rgLabel_ = $rgLabel.replace("-", "_")
-        $rgLabel_ = $rgLabel_.replace(" ", "_") 
-        $rgLevel = "AzureResourceGroups($rgLabel_, '$rgLabel', 'Azure Resource Group')" 
-        $rgLevel.replace("''", """") | Out-File -FilePath $outputFile -Append -Encoding utf8
 
         $resourceLabel = $resource.Name
         $resourceLabel_ = $resourceLabel.replace("-", "_")
         $resourceLabel_ = $resourceLabel_.replace(" ", "_")
         $resourceLabelType = $resource.ResourceType
 
+
         if ($resourceTypeIcons.ContainsKey($resourceLabelType)) {
             $getIcon = $resourceTypeIcons[$resourceLabelType]
 
-            "!includeurl AzurePuml/$($getIcon.Values)" | Out-File -FilePath $outputFile -Append -Encoding utf8
+            "!includeurl AzurePuml/$($getIcon.Values)" | Out-File -FilePath $outputFile -Append -Encoding utf8 -Force
             $resourceLevel = "$($getIcon.Keys)($resourceLabel_, '$resourceLabel', '$resourceLabelType')"
-            $resourceLevel.replace("''", """") | Out-File -FilePath $outputFile -Append -Encoding utf8
+            $resourceLevel.replace("''", """") | Out-File -FilePath $outputFile -Append -Encoding utf8 -Force
 
 
             $rel = "Rel($rgLabel_, $resourceLabel_, 'Uses', 'Resource' )"
-            $rel.replace("''", """") | Out-File -FilePath $outputFile -Append -Encoding utf8
-            "" | Out-File -FilePath $outputFile -Append -Encoding utf8
+            $rel.replace("''", """") | Out-File -FilePath $outputFile -Append -Encoding utf8 -Force
+            "" | Out-File -FilePath $outputFile -Append -Encoding utf8 -Force
         }
-
-        "LAYOUT_WITH_LEGEND()" | Out-File -FilePath $outputFile -Append -Encoding utf8
-        "@enduml" | Out-File -FilePath $outputFile -Append -Encoding utf8
     }
+
+    "LAYOUT_WITH_LEGEND()" | Out-File -FilePath $outputFile -Append -Encoding utf8 -Force
+    "@enduml" | Out-File -FilePath $outputFile -Append -Encoding utf8 -Force
 
 }
 
